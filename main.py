8498cc69375698c5565f9c36ebada5a959ff47a2
Robert Goll <robertrover811@outlook.com>
2022-09-02 15:00:52 -0700

    Create main.py
    add main code for project

---
+++ main.py
@@ -0,0 +1,39 @@
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
+
+print("Translate to Spanish, es, Translate to English, en")
+userinput = input("Enter your choice: ")
+if userinput == "es":
+    print("Enter your sentence: ")
+    sentence = input()
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt="Translate the following sentence from English to Spanish:\n" + sentence + "\n\n", 
+        temperature=0.3,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        # stop=["\n",]
+    )
+    print(response.choices[0].text)
+elif userinput == "en":
+    print("Enter your sentence: ")
+    sentence = input()
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt="Translate the following sentence from Spanish to English:\n" + sentence + "\n\n",
+        temperature=0.3,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        # stop=["\n", " Translation:"]
+    )
+    print(response.choices[0].text)
+else:
+    print("Invalid input")
+    
