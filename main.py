c32be5b6c057aac49c04ce2e32ae033495e1e6d4
Robert Goll <robertrover811@outlook.com>
2022-10-02 11:29:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,31 +9,31 @@ userinput = input("Enter your choice: ")
 if userinput == "es":
     print("Enter your sentence: ")
     sentence = input()
-    response = openai.Completion.create(
+    esresponse = openai.Completion.create(
         engine="davinci",
         prompt="Translate the following sentence from English to Spanish:\n" + sentence + "\n\n", 
         temperature=0.3,
-        max_tokens=500,
+        max_tokens=100,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
         # stop=["\n",]
     )
-    print(response.choices[0].text)
+    print(esresponse["choices"][0]["text"])
 elif userinput == "en":
     print("Enter your sentence: ")
     sentence = input()
-    response = openai.Completion.create(
+    enresponse = openai.Completion.create(
         engine="davinci",
         prompt="Translate the following sentence from Spanish to English:\n" + sentence + "\n\n",
         temperature=0.3,
-        max_tokens=500,
+        max_tokens=100,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
         # stop=["\n", " Translation:"]
     )
-    print(response.choices[0].text)
+    print(enresponse["choices"][0]["text"])
 else:
     print("Invalid input")
     
